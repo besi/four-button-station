acf466ee1aef564cd6d059dbed599153835a5daa
Beat Besmer <beat.besmer@gmail.com>
2025-03-15 12:21:25 +0100

    Add boot code for connecting to the WIFI

---
+++ sw/secrets.example.py
@@ -0,0 +1,13 @@
+class wifi:
+   aps = {
+       'SSID1': 'PASSWORD1',
+       'SSID2': 'PASSWORD2',       
+       }
+
+class telegram:
+    token = 'XYZ:ABC'
+    chat_id = 123456
+
+class mqtt:
+    host = 'mqtt://yourserver.com:1883'
+    topic = 'your.topic'
