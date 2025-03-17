1d23d960ee5be04ff1b8c86533bb31583e6f4339
Beat Besmer <beat.besmer@gmail.com>
2025-03-17 13:15:18 +0100

    Publish button presses on MQTT

--- sw/main.py
+++ sw/main.py
@@ -1,27 +1,68 @@
-import machine
+import secrets
 import time
+import ubinascii
 from machine import ADC
+from machine import Pin
+from umqtt.robust import MQTTClient 
 
-# Modified board. Radio b is connected to the ADC
-radio_a = machine.Pin(4,machine.Pin.IN)
+DELAY = 300
+
+radio_a = Pin(4,Pin.IN)
 radio_b = ADC(0)
-radio_c = machine.Pin(15,machine.Pin.IN)
-radio_d = machine.Pin(13,machine.Pin.IN)
+radio_c = Pin(15,Pin.IN)
+radio_d = Pin(13,Pin.IN)
 
-relay_1 = machine.Pin(16,machine.Pin.OUT)
-relay_2 = machine.Pin(14,machine.Pin.OUT)
-relay_3 = machine.Pin(12,machine.Pin.OUT)
-relay_4 = machine.Pin(5,machine.Pin.OUT)
+Pin(16,Pin.OUT).value(0)
+Pin(14,Pin.OUT,Pin.PULL_UP).value(0)
+Pin(12,Pin.OUT,Pin.PULL_UP).value(0)
+Pin(5,Pin.OUT,Pin.PULL_UP).value(0)
 
 
-relay_1.off()
-relay_2.off()
-relay_3.off()
-relay_4.off()
+## MQTT
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_pub = secrets.mqtt.topic
+mqtt_server = secrets.mqtt.host
+mqtt_port = secrets.mqtt.port
+mqtt_user = secrets.mqtt.user
+mqtt_password = secrets.mqtt.password
+
+def connect():
+  # if you set keepalive value - you have to send something to mqtt server to inform that you are alive in less time than keepalive value of seconds.
+  client = MQTTClient(client_id, mqtt_server, mqtt_port, mqtt_user, mqtt_password,keepalive=DELAY * 2)
+  client.set_last_will(topic_pub, '{ "status":"offline"} ', retain=False, qos=0)
+  client.connect()
+  print('Connected to %s MQTT broker' % mqtt_server)
+  client.publish(topic_pub, bytes('{"status":"hello"}', 'utf-8'))
+  return client
+
+def stamp():
+    (year,month,mday,h,m,s,weekday,yearday) = utime.localtime()
+    return f"{hour}:{minute:02d}"
+    
+    
+print(f"{stamp()} Starting up")
+client = connect()
+
+print("Connected to MQTT")
+mqtt_keepalive = time.time()
 
 while True:
-    if radio_a() == True: print("A pressed"); relay_1.value(1-relay_1.value()); time.sleep(0.5)
-    if radio_b.read()>500: print("B pressed"); relay_2.value(1-relay_2.value()); time.sleep(0.5)
-    if radio_c() == True: print("C pressed"); relay_3.value(1-relay_3.value()); time.sleep(0.5)
-    if radio_d() == True: print("D pressed"); relay_4.value(1-relay_4.value()); time.sleep(0.5)
+
+    if time.time() - mqtt_keepalive > DELAY:
+        mqtt_keepalive = time.time()
+        print(f"{stamp()} Reconnecting MQTT...")
+        client.reconnect()
+
+    button = None
+    if radio_a() == True: button = 'A'
+    if radio_b.read()>500: button = 'B'
+    if radio_c() == True: button = 'C'
+    if radio_d() == True: button = 'D'
+    
+    if button != None:
+        print(f"- {stamp()} {button} pressed >> MQTT")
+        client.publish(topic_pub, bytes('{"button":"%s"}'% button, 'utf-8'))
+        mqtt_keepalive = time.time()
+        time.sleep(0.5)
+    time.sleep(0.1)
 
